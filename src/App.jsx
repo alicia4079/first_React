d5998a3010ded5ccab76f142abed931260e86d58
alicia4079 <136130096+alicia4079@users.noreply.github.com>
2024-02-23 22:00:37 +0100

    .

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import ShowCount from './components/ShowCount'
+
+function App() {
+  return (
+    <div className='App'>
+      <ShowCount initialCount={0} />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/ShowCount.jsx
@@ -0,0 +1,27 @@
+import './ShowCount.css'
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react'
+
+// eslint-disable-next-line react/prop-types
+const ShowCount = ({ initialCount }) => {
+  const [count, setCount] = useState(initialCount)
+
+  const restar = () => {
+    setCount(count - 1)
+  }
+
+  const sumar = () => {
+    setCount(count + 1)
+  }
+  return (
+    <div>
+      <h2>You clicked here {count} times</h2>
+      <div className='buttons'>
+        <button onClick={restar}>Restar</button>
+        <button onClick={sumar}>Sumar</button>
+      </div>
+    </div>
+  )
+}
+
+export default ShowCount
